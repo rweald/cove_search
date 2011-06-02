a1d9c59d2d36401e8dc2708253f948e94b2125ac
Ryan Weald <ryan@weald.com>
2011-06-01 23:03:48 -0700

    add the ability to increment by a custom value

--- lib/cove_search/index.rb
+++ lib/cove_search/index.rb
@@ -20,9 +20,9 @@ module CoveSearch
     #add an item to our index. The redis key will be scoped by type so the key will have the form
     # type:query. The content stored in the set will be member which represents the info needed 
     # to retrieve the full document on the application server
-    def self.add(type=nil, query=nil, member=nil)
+    def self.add(type=nil, query=nil, member=nil, incremental=1)
       raise "Missing Constraints. Must specify a type,query, or member" unless (type && query && member)
-      @redis.zincrby(redis_key(type, query), 1, member)
+      @redis.zincrby(redis_key(type, query), incremental, member)
     end
 
     def self.redis_key(type, query)
