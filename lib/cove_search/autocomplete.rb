468a6da63049f285f3a1305442358d7bcbf5691d
Ryan Weald <ryan@weald.com>
2011-06-04 22:31:25 -0700

    Make all the autocomplete stems namespaced by type

--- lib/cove_search/autocomplete.rb
+++ lib/cove_search/autocomplete.rb
@@ -15,19 +15,19 @@ module CoveSearch
     
     @redis = Redis::Namespace.new "search_index", :redis => rc
 
-    def self.generate_ngram_index_for_word(word)
+    def self.generate_ngram_index_for_word(word, type)
       (2..word.length).each do |i|
         suffix = word[0...i]
-        @redis.zincrby(redis_key(suffix), 1, word)
+        @redis.zincrby(redis_key(suffix, type), 1, word)
       end
     end
 
-    def self.autocomplete_for_suffix(suffix, limit=5)
-      @redis.zrange(redis_key(suffix), 0, limit)
+    def self.autocomplete_for_suffix(suffix, type, limit=5)
+      @redis.zrange(redis_key(suffix, type), 0, limit)
     end
 
-    def self.redis_key(suffix)
-      "autocomplete:#{suffix}"
+    def self.redis_key(suffix, type)
+      "autocomplete:#{type}:#{suffix}"
     end
 
   end
--- spec/cove_search/autocomplete_spec.rb
+++ spec/cove_search/autocomplete_spec.rb
@@ -6,46 +6,45 @@ describe "CoveSearch" do
     describe ".generate_ngram_index_for_word" do
       it "should create the necessary stubs if they dont already exist" do
         AutoComplete.redis.exists("autocomplete:hel").should be_false
-        AutoComplete.generate_ngram_index_for_word("hello")
-        AutoComplete.redis.exists("autocomplete:hel").should be_true
-        AutoComplete.redis.exists("autocomplete:hell").should be_true
-        AutoComplete.redis.exists("autocomplete:hello").should be_true
+        AutoComplete.generate_ngram_index_for_word("hello", "test:tag")
+        AutoComplete.redis.exists("autocomplete:test:tag:hel").should be_true
+        AutoComplete.redis.exists("autocomplete:test:tag:hell").should be_true
+        AutoComplete.redis.exists("autocomplete:test:tag:hello").should be_true
       end
 
       it "should add another word to the set if key already exists" do
-        AutoComplete.generate_ngram_index_for_word("hello")
-        AutoComplete.generate_ngram_index_for_word("helly")
-        AutoComplete.redis.zrange("autocomplete:hel", 0, 2).should == ["hello", "helly"]
+        AutoComplete.generate_ngram_index_for_word("hello", "test:tag")
+        AutoComplete.generate_ngram_index_for_word("helly", "test:tag")
+        AutoComplete.redis.zrange("autocomplete:test:tag:hel", 0, 2).should == ["hello", "helly"]
       end
 
       it "should handle spaces like any other character" do
-        AutoComplete.generate_ngram_index_for_word("hello world")
-        AutoComplete.redis.zrange("autocomplete:hello", 0, 2).should == ["hello world"]
-        AutoComplete.redis.zrange("autocomplete:hello ", 0, 2).should == ["hello world"]
-        AutoComplete.redis.zrange("autocomplete:hello w", 0, 2).should == ["hello world"]
-               
+        AutoComplete.generate_ngram_index_for_word("hello world", "test:tag")
+        AutoComplete.redis.zrange("autocomplete:test:tag:hello", 0, 2).should == ["hello world"]
+        AutoComplete.redis.zrange("autocomplete:test:tag:hello ", 0, 2).should == ["hello world"]
+        AutoComplete.redis.zrange("autocomplete:test:tag:hello w", 0, 2).should == ["hello world"]
       end
     end
 
     describe ".autocomplete_for_suffix" do
         
       it "should return all the words with the given suffix" do
-        AutoComplete.generate_ngram_index_for_word("hello")
-        AutoComplete.autocomplete_for_suffix("hel").should == ["hello"]
+        AutoComplete.generate_ngram_index_for_word("hello", "test:tag")
+        AutoComplete.autocomplete_for_suffix("hel", "test:tag").should == ["hello"]
 
-        AutoComplete.generate_ngram_index_for_word("helly")
-        AutoComplete.autocomplete_for_suffix("hel").should == ["hello", "helly"]
+        AutoComplete.generate_ngram_index_for_word("helly", "test:tag")
+        AutoComplete.autocomplete_for_suffix("hel", "test:tag").should == ["hello", "helly"]
       end
 
       it "should return an empty array if nothing is found" do
-        AutoComplete.autocomplete_for_suffix("blah").should == []
+        AutoComplete.autocomplete_for_suffix("blah", "test:tag").should == []
       end
     end
     
     describe ".redis_key" do
       it "should return the autocomplete namespaced key" do
-        AutoComplete.redis_key("test").should == "autocomplete:test"
-        AutoComplete.redis_key("").should == "autocomplete:"
+        AutoComplete.redis_key("test", "tag:hello").should == "autocomplete:tag:hello:test"
+        AutoComplete.redis_key("", "test:tag").should == "autocomplete:test:tag:"
       end
     end
   end
